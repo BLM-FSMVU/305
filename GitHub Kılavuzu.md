f0ce2b734071fe32708a23d8a96944972aa7b32d
M Akif Eyler <ma.eyler@gmail.com>
2020-10-13 07:05:20 +0300

    chap4 objects

---
+++ GitHub Kılavuzu.md
@@ -0,0 +1,67 @@
+﻿## GitHub Kılavuzu
+
+GitHub kullanmak için üç önemli sebep:
+* Projeniz bir bütün olarak bulutta, her yerden erişilebilir
+* Otomatik sürüm kontrolü ve koordineli takım çalışması 
+* Programcılar için özgeçmişin önemli bir parçası
+
+Git'in akış ve dallanma mantığını anlamak için iyi bir başlangıç [burada](https://guides.github.com/introduction/flow/)
+
+### GitKraken
+
+Bu program ile Git kullanmak çok kolaylaştı
+
+Öncelikle [GitHub hesabı açmak](https://github.com/join)  gerekiyor
+
+
+### Commit, Branch, Pull Request, Merge
+
+Yerel bilgisayarda denemeleri yapıp çalışır duruma getirince `Commit` işlemini yapıyoruz. Dokümanlardaki "Kaydet" gibi bir işlem, tek farkı, bütün ayrıntılar kaydedildiği için eski bir duruma dönmek mümkün. `Commit` işlemlerinin çok sayıda olmasında korkmayın: Her Commit, programın bir tek özelliği üstünde çalışsa iyi olur.
+
+Yeni bir özellik denemek ya da takım çalışması yapmak için [dallanma](https://guides.github.com/introduction/flow) gerekiyor. Her dalı ayrı bir proje olarak düşünebiliriz. Desktop programında dal değiştirince, klasördeki dosyalar da uygun bir şekilde değişiyor, sadece o dala ait dosyalar görünüyor. Programın ana dalına `master` adı verilmiş. 
+
+Bir dal üstünde yeteri kadar çalışıp doğru işlediği görülünce, o dalın sahibi `Pull Request` yapıyor, projenin sahibi ya `Merge` ile yeni özellikleri programa ekliyor, ya da dalı silerek reddediyor. 
+
+### Markdown (MD) formatı
+
+Karmaşık HTML sayfalarını basitleştiren [MD formatını](https://guides.github.com/features/mastering-markdown/#examples) on dakikada öğrenmek ister misiniz? GitHub'da bu formatı kullanmak şart değil ama bunun gibi sayfaları düz metin gibi kolayca yazabilirsiniz.
+
+![örnekler sayfası](images/examples.PNG)
+
+Markdown denemeleriniz için [bir çevirmen](http://dillinger.io/) yararlı olacak
+
+
+### Git Command Line (CLI)
+
+GitKraken öncesinde, bu adımları *Command line* içinde yerel klasör altında `git` komutu ile yapmak gerekiyordu
+
+GitKraken'deki her işleme karşı gelen `git` komutu ile kıyaslayınca sürecin ne kadar basitleştiğini görebilirsiniz:
+* File menüsünde Init Repo: `git init`
+* `Stage all changes` düğmesine basın: `git add *` 
+* `Commit` düğmesine basın: `git commit -m "yeni bir isim"`
+* Remote ekleyin: `git remote add origin https://github.com/maeyler/BLM305.git`
+* `Push` düğmesine basın: `git push origin master`
+
+Git için [kapsamlı bir kitap](https://git-scm.com/book/en/v2) var ama GitKraken yazılımı normal bir kullanıcının çoğu ihtiyacını karşılıyor. <br>
+"Merge conflict" ya da "merge revert" durumlarında biraz Git bilmek gerekecektir.
+
+
+### Referanslar
+
+* https://forvo.com/word/github/
+* https://www.wikiwand.com/en/Git
+* https://github.com
+* https://guides.github.com
+* https://guides.github.com/introduction/flow
+* https://dillinger.io
+* https://try.github.io
+
+
+### Teşekkürler
+
+* Gürsu Gülcü 
+* [Hakan DILEK](https://github.com/hakandilek)
+* Ayla Gülcü
+* [Güngör Yılmaz](https://github.com/gungor)
+ 
+Eski öğrencilerim bu konuda bana öğretmen oldular :smile:
---
+++ work/chap4.js
@@ -0,0 +1,143 @@
+﻿function phi([n00, n01, n10, n11]) {
+  return (n11 * n00 - n10 * n01) /
+    Math.sqrt((n10 + n11) * (n00 + n01) *
+              (n01 + n11) * (n00 + n10));
+}
+
+function tableFor(evt) {
+  let table = [0, 0, 0, 0];
+  for (let entry of JOURNAL) {
+    let index = 0;
+    if (entry.events.includes(evt)) index += 1;
+    if (entry.squirrel) index += 2;
+    table[index] += 1;
+  }
+  return table;
+}
+
+function analyze(min=0) {
+  let a = [];
+  for (let evt of EVENTS) {
+    let cor = phi(tableFor(evt));
+    if (Math.abs(cor) > min)
+      a.push(evt +": "+cor.toFixed(4))
+  }
+  return a
+}
+
+function journalEvents() {
+  EVENTS.clear();
+  for (let entry of JOURNAL)
+    for (let e of entry.events)
+      EVENTS.add(e);
+}
+
+
+
+class Entry {
+  constructor(evt, sq) {
+    this.events = evt; this.squirrel = sq
+  }
+  toString() {
+    let s = this.events.length;
+    if (this.squirrel) s += " ✘";
+    return "[object Entry] " + s
+  }
+}
+
+const EVENTS = new Set();  //use Set -- not Array
+
+const JOURNAL = [
+  new Entry(["carrot","exercise","weekend"], false),
+  new Entry(["bread","pudding","brushed teeth","weekend","touched tree"], false),
+  new Entry(["carrot","nachos","brushed teeth","cycling","weekend"], false),
+  new Entry(["brussel sprouts","ice cream","brushed teeth","computer","weekend"], false),
+  new Entry(["potatoes","candy","brushed teeth","exercise","weekend","dentist"], false),
+  new Entry(["brussel sprouts","pudding","brushed teeth","running","weekend"], false),
+  new Entry(["pizza","brushed teeth","computer","work","touched tree"], false),
+  new Entry(["bread","beer","brushed teeth","cycling","work"], false),
+  new Entry(["cauliflower","brushed teeth","work"], false),
+  new Entry(["pizza","brushed teeth","cycling","work"], false),
+  new Entry(["lasagna","nachos","brushed teeth","work"], false),
+  new Entry(["brushed teeth","weekend","touched tree"], false),
+  new Entry(["lettuce","brushed teeth","television","weekend"], false),
+  new Entry(["spaghetti","brushed teeth","work"], false),
+  new Entry(["brushed teeth","computer","work"], false),
+  new Entry(["lettuce","nachos","brushed teeth","work"], false),
+  new Entry(["carrot","brushed teeth","running","work"], false),
+  new Entry(["brushed teeth","work"], false),
+  new Entry(["cauliflower","reading","weekend"], false),
+  new Entry(["bread","brushed teeth","weekend"], false),
+  new Entry(["lasagna","brushed teeth","exercise","work"], false),
+  new Entry(["spaghetti","brushed teeth","reading","work"], false),
+  new Entry(["carrot","ice cream","brushed teeth","television","work"], false),
+  new Entry(["spaghetti","nachos","work"], false),
+  new Entry(["cauliflower","ice cream","brushed teeth","cycling","work"], false),
+  new Entry(["spaghetti","peanuts","computer","weekend"], true),
+  new Entry(["potatoes","ice cream","brushed teeth","computer","weekend"], false),
+  new Entry(["potatoes","ice cream","brushed teeth","work"], false),
+  new Entry(["peanuts","brushed teeth","running","work"], false),
+  new Entry(["potatoes","exercise","work"], false),
+  new Entry(["pizza","ice cream","computer","work"], false),
+  new Entry(["lasagna","ice cream","work"], false),
+  new Entry(["cauliflower","candy","reading","weekend"], false),
+  new Entry(["lasagna","nachos","brushed teeth","running","weekend"], false),
+  new Entry(["potatoes","brushed teeth","work"], false),
+  new Entry(["carrot","work"], false),
+  new Entry(["pizza","beer","work","dentist"], false),
+  new Entry(["lasagna","pudding","cycling","work"], false),
+  new Entry(["spaghetti","brushed teeth","reading","work"], false),
+  new Entry(["spaghetti","pudding","television","weekend"], false),
+  new Entry(["bread","brushed teeth","exercise","weekend"], false),
+  new Entry(["lasagna","peanuts","work"], true),
+  new Entry(["pizza","work"], false),
+  new Entry(["potatoes","exercise","work"], false),
+  new Entry(["brushed teeth","exercise","work"], false),
+  new Entry(["spaghetti","brushed teeth","television","work"], false),
+  new Entry(["pizza","cycling","weekend"], false),
+  new Entry(["carrot","brushed teeth","weekend"], false),
+  new Entry(["carrot","beer","brushed teeth","work"], false),
+  new Entry(["pizza","peanuts","candy","work"], true),
+  new Entry(["carrot","peanuts","brushed teeth","reading","work"], false),
+  new Entry(["potatoes","peanuts","brushed teeth","work"], false),
+  new Entry(["carrot","nachos","brushed teeth","exercise","work"], false),
+  new Entry(["pizza","peanuts","brushed teeth","television","weekend"], false),
+  new Entry(["lasagna","brushed teeth","cycling","weekend"], false),
+  new Entry(["cauliflower","peanuts","brushed teeth","computer","work","touched tree"], false),
+  new Entry(["lettuce","brushed teeth","television","work"], false),
+  new Entry(["potatoes","brushed teeth","computer","work"], false),
+  new Entry(["bread","candy","work"], false),
+  new Entry(["potatoes","nachos","work"], false),
+  new Entry(["carrot","pudding","brushed teeth","weekend"], false),
+  new Entry(["carrot","brushed teeth","exercise","weekend","touched tree"], false),
+  new Entry(["brussel sprouts","running","work"], false),
+  new Entry(["brushed teeth","work"], false),
+  new Entry(["lettuce","brushed teeth","running","work"], false),
+  new Entry(["candy","brushed teeth","work"], false),
+  new Entry(["brussel sprouts","brushed teeth","computer","work"], false),
+  new Entry(["bread","brushed teeth","weekend"], false),
+  new Entry(["cauliflower","brushed teeth","weekend"], false),
+  new Entry(["spaghetti","candy","television","work","touched tree"], false),
+  new Entry(["carrot","pudding","brushed teeth","work"], false),
+  new Entry(["lettuce","brushed teeth","work"], false),
+  new Entry(["carrot","ice cream","brushed teeth","cycling","work"], false),
+  new Entry(["pizza","brushed teeth","work"], false),
+  new Entry(["spaghetti","peanuts","exercise","weekend"], true),
+  new Entry(["bread","beer","computer","weekend","touched tree"], false),
+  new Entry(["brushed teeth","running","work"], false),
+  new Entry(["lettuce","peanuts","brushed teeth","work","touched tree"], false),
+  new Entry(["lasagna","brushed teeth","television","work"], false),
+  new Entry(["cauliflower","brushed teeth","running","work"], false),
+  new Entry(["carrot","brushed teeth","running","work"], false),
+  new Entry(["carrot","reading","weekend"], false),
+  new Entry(["carrot","peanuts","reading","weekend"], true),
+  new Entry(["potatoes","brushed teeth","running","work"], false),
+  new Entry(["lasagna","ice cream","work","touched tree"], false),
+  new Entry(["cauliflower","peanuts","brushed teeth","cycling","work"], false),
+  new Entry(["pizza","brushed teeth","running","work"], false),
+  new Entry(["lettuce","brushed teeth","work"], false),
+  new Entry(["bread","brushed teeth","television","weekend"], false),
+  new Entry(["cauliflower","peanuts","brushed teeth","weekend"], false)
+]
+
+journalEvents()  //determine the set of events
