9a11791ee57082fdf462afd100a8924735c0c20b
M Akif Eyler <ma.eyler@gmail.com>
2020-11-30 08:36:54 +0300

    anim folder for HW3

---
+++ anim/Clock_Small.js
@@ -0,0 +1,16 @@
+class Clock_Small extends Animator  {
+    constructor() {
+        super();
+        this.author = 'Small Simple'
+        this.description = 'Simple digital clock'
+        let x0 = "translate(0)", x1 = "translate(300px)"
+        this.anim = { transform: [x0, x1, x0] }
+        this.elt = document.createElement('p')
+        this.elt.style = "font: 35px serif;"
+        this.time = 3000
+    }
+    update() {
+        let [d] = new Date().toTimeString().split(' ')
+        this.elt.innerText = '\n\n'+d
+    }
+}
---
+++ anim/Color_Eyler.js
@@ -0,0 +1,13 @@
+class Color_Eyler extends Animator  {
+    constructor() {
+        super();
+        this.author = 'M A Eyler'
+        this.description = 'Smooth color change'
+        let b = ["lightcyan", "magenta", "yellow", "lightcyan"]
+        this.anim = { background: b }
+        this.elt = document.createElement('div')
+        this.elt.style = "width: 100%; height: 100%;"
+        this.time = 6000
+    }
+    update() { }  //no code is needed here
+}
