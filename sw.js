6533e0d3f7bc4d21dd98047b59f0a47a9dbc6348
M Akif Eyler <ma.eyler@gmail.com>
2020-12-01 10:39:04 +0300

    make PWA

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE ='AP'
+function installCB(e) {
+  console.log(CACHE, e);
+}
+addEventListener('install', installCB)
+
+function save(req, resp) {
+  if (!req.url.includes("github")) 
+     return resp;
+  return caches.open(CACHE)
+  .then(cache => { // save request
+    cache.put(req, resp.clone());
+    return resp;
+  }) 
+  .catch(console.err)
+}
+function report(req) {
+  console.log(CACHE+' matches '+req.url)
+  return req
+}
+function fetchCB(e) { //fetch first
+  let req = e.request
+  e.respondWith(
+    fetch(req).then(r2 => save(req, r2))
+    .catch(() => caches.match(req).then(report))
+  )
+}
+addEventListener('fetch', fetchCB)
+
+function activateCB(e) {
+  console.log(CACHE, e);
+}
+addEventListener('activate', activateCB);
+
